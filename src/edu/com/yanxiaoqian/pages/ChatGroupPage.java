970bd26eaa48d6ae6d7c9569e92674cab4850182
yanxiaoqian <728710545@qq.com>
2020-05-07 16:05:07 +0800

    aaa

--- src/edu/com/yanxiaoqian/pages/ChatGroupPage.java
+++ src/edu/com/yanxiaoqian/pages/ChatGroupPage.java
@@ -32,6 +32,43 @@ public class ChatGroupPage {
 		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[2]");
 	}
 	
+	private AndroidElement right1() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]");
+	}
+	
+	private AndroidElement right2() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]");
+	}
+	
+	private AndroidElement right3() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]");
+	}
+	
+	private AndroidElement right4() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[4]");
+	}
+	
+	private AndroidElement cancel() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText/android.widget.Button[1]");
+	}
+	
+	private AndroidElement redefine() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText/android.widget.Button[2]");
+	}
+	
+	
+	private AndroidElement retext() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText");
+	}
+	
+	private AndroidElement person() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]");
+	}
+	
+	private AndroidElement star() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[1]/android.widget.Button[2]");
+	}
+	
 	public void add(String s) throws InterruptedException {
 		Thread.sleep(3000);
 		action.click(chatGroup());
@@ -47,6 +84,105 @@ public class ChatGroupPage {
 		Thread.sleep(3000);
 		action.click(chat1());
 		action.click(submit());
+	}
+	
+	public void freshen() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right4());
+	}
+	
+	public void rename(String s) throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.type(retext(),s);
+		action.click(redefine());
+	}
+	
+	public void renameWhite() throws InterruptedException{
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(redefine());
+	}
+	
+	public void collect() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right1());
+	}
+	
+	public void deleteCollect() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right1());
+	}
+	
+	public void viewList() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+	}
+	
+	public void collectPerson() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(person());
+		action.click(star());
+	}
+	
+	public void deletePerson() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(person());
+		action.click(star());
 	}
 
 }
--- src/edu/com/yanxiaoqian/textcase/ChatGroupTest.java
+++ src/edu/com/yanxiaoqian/textcase/ChatGroupTest.java
@@ -46,5 +46,71 @@ public class ChatGroupTest extends BaseTest{
 		ChatGroupPage submit = new ChatGroupPage(driver);
 		submit.add("毒品");
 	}
+	
+	@Test
+	public void test7() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.freshen();
+	}
+	
+	@Test
+	public void test8() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.rename("讨论组一");
+	}
+	
+	@Test
+	public void test9() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.renameWhite();
+	}
+	
+	@Test
+	public void test10() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.rename("aaa");
+	}
+	
+	@Test
+	public void test11() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.rename("111");
+	}
+	
+	@Test
+	public void test12() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.rename("毒品");
+	}
+	
+	@Test
+	public void test13() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.collect();
+	}
+	
+	@Test
+	public void test14() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.deleteCollect();
+	}
+	
+	@Test
+	public void test15() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.viewList();
+	}
+	
+	@Test
+	public void test17() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.collectPerson();
+	}
+	
+	@Test
+	public void test18() throws InterruptedException{
+		ChatGroupPage submit = new ChatGroupPage(driver);
+		submit.deletePerson();
+	}
 
 }
