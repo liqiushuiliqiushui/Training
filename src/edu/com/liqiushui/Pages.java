3ff614fa54d9ff69b3995c8bfd08a9e663516d32
liqiushuiliqiushui <496629735@qq.com>
2020-05-23 20:20:47 +0800

    a

--- src/edu/com/liqiushui/Pages.java
+++ src/edu/com/liqiushui/Pages.java
@@ -1,20 +1,68 @@
 package edu.com.liqiushui;
 
 import edu.basetest.BaseTest;
+import edu.util.BaseAction;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class Pages extends BaseTest{
+
+	private AndroidDriver<AndroidElement> driver;
+	BaseAction action;
+	public Pages(AndroidDriver<AndroidElement> driver) {
+		this.driver = driver;
+		this.action = new BaseAction(driver);
+	}
+	public AndroidElement my() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View");
+	}
+	public AndroidElement list() {
+		return driver.findElementByXPath("//*[@text='显示菜单']");
+	}
 	public AndroidElement chat() {
 		return driver.findElementByXPath("//*[@text='聊天']");
+	}
+	public AndroidElement taolun() {
+		return driver.findElementByXPath("//*[@text='讨论组']");
+	}
+	public AndroidElement lianxi() {
+		return driver.findElementByXPath("//*[@text='联系人']");
+	}
+	public AndroidElement mine() {
+		return driver.findElementByXPath("//*[@text='我的']");
 	}
 	public AndroidElement creat() {
 		return driver.findElementByXPath("//*[@text='创建聊天']");
 	}
-	public AndroidElement my() {
-		return driver.findElementByXPath("//*[@text='我的']");
+	public AndroidElement adda() {
+		return driver.findElementByXPath("//*[@text='admin']");
 	}
-	public AndroidElement list() {
+	public AndroidElement addlqs() {
+		return driver.findElementByXPath("//*[@text='李秋水1']");
+	}
+	
+	public AndroidElement xitong() {
+		return driver.findElementByXPath("//*[@text='系统\r\n" + 
+				"所有人']");
+	}
+	public AndroidElement menu() {
 		return driver.findElementByXPath("//*[@text='显示菜单']");
+	}
+	public AndroidElement starr() {
+		return driver.findElementByXPath("//*[@text='收藏']");
+	}
+	public AndroidElement unstar() {
+		return driver.findElementByXPath("//*[@text='取消收藏']");
+	}
+	public AndroidElement people() {
+		return driver.findElementByXPath("//*[@text='成员列表']");
+	}
+	
+	public AndroidElement news() {
+		return driver.findElementByXPath("//*[@text='同步最近的消息']");
+	}
+	public AndroidElement rename() {
+		return driver.findElementByXPath("//*[@text='重命名']");
 	}
 	public AndroidElement about() {
 		return driver.findElementByXPath("//*[@text='关于']");
@@ -29,7 +77,7 @@ public class Pages extends BaseTest{
 		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View");
 		
 	}
-	public AndroidElement cancel() {
+	public AndroidElement cancel2() {
 		return driver.findElementByXPath("//*[@text='注销']");
 	}
 	public AndroidElement enter1() {
@@ -38,4 +86,177 @@ public class Pages extends BaseTest{
 	public AndroidElement nochose() {
 		return driver.findElementByXPath("//*[@text='取消']");
 	}
+	private AndroidElement chatGroup() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]/android.view.View[2]");
+	}
+	
+	private AndroidElement chat1() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]");
+	}
+	
+	private AndroidElement text() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText");
+	}
+	
+	private AndroidElement submit() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button[2]");
+	}
+	
+	private AndroidElement right() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[2]");
+	}
+	
+	private AndroidElement right1() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]");
+	}
+	
+	private AndroidElement right2() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]");
+	}
+	
+	private AndroidElement right3() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[3]");
+	}
+	
+	private AndroidElement right4() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[4]");
+	}
+	
+	private AndroidElement cancel() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText/android.widget.Button[1]");
+	}
+	
+	private AndroidElement redefine() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText/android.widget.Button[2]");
+	}
+	
+	
+	private AndroidElement retext() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText");
+	}
+	
+	private AndroidElement person() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]");
+	}
+	
+	private AndroidElement star() {
+		return driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[1]/android.widget.Button[2]");
+	}
+	
+	public void add(String s) throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		action.type(text(),s);
+		action.click(submit());
+	}
+	
+	public void addWhite() throws InterruptedException{
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		action.click(submit());
+	}
+	
+	public void freshen() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right4());
+	}
+	
+	public void rename(String s) throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.type(retext(),s);
+		action.click(redefine());
+	}
+	
+	public void renameWhite() throws InterruptedException{
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(redefine());
+	}
+	
+	public void collect() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right1());
+	}
+	
+	public void deleteCollect() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right1());
+	}
+	
+	public void viewList() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+	}
+	
+	public void collectPerson() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(person());
+		action.click(star());
+	}
+	
+	public void deletePerson() throws InterruptedException {
+		Thread.sleep(3000);
+		action.click(chatGroup());
+		Thread.sleep(3000);
+		action.click(chat1());
+		Thread.sleep(3000);
+		action.click(right());
+		Thread.sleep(3000);
+		action.click(right3());
+		Thread.sleep(3000);
+		action.click(person());
+		action.click(star());
+	}
+
 }
--- src/edu/com/liqiushui/TestCase.java
+++ src/edu/com/liqiushui/TestCase.java
@@ -1,23 +1,135 @@
 package edu.com.liqiushui;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import edu.basetest.BaseTest;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class TestCase extends Pages{
-
+public class TestCase extends BaseTest{
+	Pages pages=new Pages(driver);
 	//点击聊天，页面是否跳转
-		@Test
-		public void clickchat() {
-			chat().click();
-		}
-	//点击创建聊天
-		@Test
-		public void createchat() {
-			creat().click();
-		}
+	@Test
+	public void clickchat() {
+		pages.chat().click();
+	}
+	//点击我的，页面是否跳转
+	@Test
+	public void dump() {
+		pages.my().click();
+	}
+	//点击联系人，页面是否跳转
+	@Test
+	public void clicklianxi() {
+		pages.lianxi().click();
+				
+	}
+	//收藏联系人
+	@Test
+	public void starr() {
+		pages.xitong().click();
+		pages.menu().click();
+		pages.starr().click();
+				
+	}
+	//取消收藏联系人
+	@Test
+	public void unstar() {
+		pages.menu().click();
+		pages.unstar().click();
+				
+	}
+	
+	//查看成员列表
+			@Test
 
+	public void seeNum() {
+		pages.menu().click();
+		pages.people().click();
+	}
+			
+	//同步最近的消息		
+	@Test
 
+	public void news() {
+		pages.menu().click();
+		pages.news().click();	
+	}
+    //重命名点击
+	@Test
+	public void newname() {
+		pages.menu().click();
+		pages.rename().click();
+				
+	}
+	//同步最近的消息
+	public void newmess() {
+	   pages.menu().click();
+	   pages.news().click();		
+	}
+	
 
+	@Test
+	public void test10() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.renameWhite();
+	}
+	
+	@Test
+	public void test11() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.rename("aaa");
+	}
+	
+	@Test
+	public void test12() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.rename("111");
+	}
+	
+	@Test
+	public void test13() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.rename("毒品");
+	}
+	
+	@Test
+	public void test14() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.add("");
+	}
+	
+	@Test
+	public void test15() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.add("毒品");
+	}
+	
+	@Test
+	public void test16() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.deleteCollect();
+	}
+	
+	@Test
+	public void test17() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.viewList();
+	}
+	
+	@Test
+	public void test18() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.collectPerson();
+	}
+	
+	@Test
+	public void test19() throws InterruptedException{
+		Pages submit = new Pages(driver);
+		submit.deletePerson();
+	}
 }
